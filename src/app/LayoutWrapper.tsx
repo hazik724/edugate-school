277ca75ade3ad3451906410bc5b6a0c632b40ab5
hazik724 <hazikaslamqaimkhani@gmail.com>
2026-04-06 03:18:50 +0500

    content remaing

---
+++ src/app/LayoutWrapper.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { usePathname } from "next/navigation"
+import Navbar from "@/components/Navbar"
+import Footer from "@/components/Footer"
+
+export default function LayoutWrapper({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname()
+
+  const isStudio = pathname.startsWith("/studio")
+
+  return (
+    <>
+      {!isStudio && <Navbar />}
+      <main className="flex-1">{children}</main>
+      {!isStudio && <Footer />}
+    </>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+import LayoutWrapper from "./LayoutWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,16 +32,13 @@ export default function RootLayout({
       <body className="min-h-screen flex flex-col">
         
         {/* Navbar */}
-        <Navbar />
+        <LayoutWrapper >
 
         {/* Page Content */}
         <main className="flex-1">
           {children}
         </main>
-
-        {/* Footer */}
-        <Footer />
-
+</LayoutWrapper>
       </body>
     </html>
   );
