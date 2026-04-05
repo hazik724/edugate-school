3417df582563476e07eeb860d0ee355ac6532226
hazik724 <hazikaslamqaimkhani@gmail.com>
2026-04-06 03:51:35 +0500

    cache store

--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -1,20 +1,28 @@
 import { client } from "./client";
 
 export async function getAnnouncements() {
-  return await client.fetch(`
-    *[_type == "announcement"] | order(_createdAt desc)
-  `);
+  return await client.fetch(
+    `*[_type == "announcement"] | order(_createdAt desc)`,
+    {},
+    { cache: "no-store" }
+  );
 }
+
 export async function getGallery() {
-    return await client.fetch(`
-      *[_type == "gallery"] | order(_createdAt desc){
+  return await client.fetch(
+    `*[_type == "gallery"] | order(_createdAt desc){
       image,
       video
-      }
-    `);
-  }
-  export async function getTestimonials() {
-    return await client.fetch(`
-      *[_type == "testimonial"] | order(_createdAt desc)
-    `);
-  }
+    }`,
+    {},
+    { cache: "no-store" }
+  );
+}
+
+export async function getTestimonials() {
+  return await client.fetch(
+    `*[_type == "testimonial"] | order(_createdAt desc)`,
+    {},
+    { cache: "no-store" }
+  );
+}
